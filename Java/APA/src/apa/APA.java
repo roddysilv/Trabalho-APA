403fa2f0266bbf7c4cb16d833267672e681e881b
roddysilv <42419004+roddysilv@users.noreply.github.com>
2019-07-12 11:55:30 -0300

    Add files via upload

--- Java/APA/src/apa/APA.java
+++ Java/APA/src/apa/APA.java
@@ -5,6 +5,8 @@
  */
 package apa;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 
 /**
@@ -17,21 +19,30 @@ public class APA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int tam = 10000;
+        int tam = 50000;
         Lista l = new Lista(tam);
         //l.imprime();
    
-        l.shuffle(3);
+        l.shuffle(1,20);
         //l.imprime();
      
         Lomuto lo = new Lomuto();
         Hoare h = new Hoare();
         MedianaDe3 m3= new MedianaDe3();
+        Iterativo ite = new Iterativo();
+        Raul r = new Raul();
+        
+        Instant inicio = Instant.now();
         
         h.Hoare(l.lista);
+        Instant fim = Instant.now();
         //m3.MedianaDe3(l.lista);
         //lo.Lomuto(l.lista);
-        l.imprime();
+        //ite.Iterativo(l.lista);
+        //r.quickRaul(l.lista);
+        //l.imprime();
+        long tempo = Duration.between(inicio, fim).toNanos();
+        System.out.println(tempo + " ns");
         //for (int i = 0; i < arr.size(); i++) {
         //    System.out.print(arr.get(i) + " ");
         //}
--- Java/APA/src/apa/Lista.java
+++ Java/APA/src/apa/Lista.java
@@ -24,26 +24,34 @@ public class Lista {
         }
     }
 
-    public void shuffle(int t) {
+    public void shuffle(int t,int n) {
         Random r = new Random();
         switch (t) {
-            case 1:
-                Collections.shuffle(this.lista);
+            case 1://EMBARALHA TUDO
+                Collections.shuffle(this.lista); 
                 break;
-            case 2:
+            case 2://UMA TROCA - TROCA UMA POSICAO COM A PROXIMA
                 int aux = r.nextInt(lista.size() - 2);
                 Collections.swap(lista, aux, aux + 1);
                 break;
-            case 3:
+            case 3: // UMA TROCA - TROCA UMA POSICAO COM OUTRA ALEATORIA
                 Collections.swap(lista, r.nextInt(lista.size() - 2), r.nextInt(lista.size() - 2));
                 break;
-            case 4:
+            case 4: //N TROCAS
+                for(int i=0;i<n;i++){
+                    Collections.swap(lista, r.nextInt(lista.size() - 2), r.nextInt(lista.size() - 2));
+                }
                 break;
             default:
                 break;
         }
     }
 
+    public void umaTroca(){
+        int tam = this.lista.size()/2;
+        Collections.swap(lista, tam, tam+1);
+    }
+    
     public void imprime() {
         for (int i = 0; i < this.lista.size(); i++) {
             System.out.print(this.lista.get(i) + " ");
